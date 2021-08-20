42b08e7a20d96ade39b37e86954d79dcb46ebae3
dclemon <63597032+dclemon@users.noreply.github.com>
2021-08-20 11:58:43 +0800

    修复价格返回空值报错

--- jdtry.py
+++ jdtry.py
@@ -268,10 +268,10 @@ class jdtry:
         ex_price = read_ini('appbase', 'ex_price','info.ini')
         plan = []
         for temp in product_list:
-        try:
-            print("temp.price:",float(temp.price),"ex_price:",ex_price)
-        except:
-            continue
+            try:
+                print("temp.price:",float(temp.price),"ex_price:",ex_price)
+            except:
+                continue
         for temp in plan:
             for key_word in key_word_list:
                 if temp.name.find(key_word) != -1:  # 有过滤词
